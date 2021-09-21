4ab45e70cc50a4c1b0d1e56d6809fd583cd7e97a
dmhlupin <45971646+dmhlupin@users.noreply.github.com>
2021-09-21 21:51:43 +0300

    Add files via upload

---
+++ hw3-6/index.php
@@ -0,0 +1,18 @@
+<?php
+$menu = 
+[
+    ["href" => "index.php", "item" => "Главная"],
+    ["href" => "catalog.php", "item" => "Каталог"],
+    ["href" => "about.php", "item" => "О нас"],
+];
+
+function renderMenu($arr) {
+    $resultStr = "<ul>";
+    foreach($arr as $value) {
+        $resultStr .= "<li><a href = {$value['href']}>{$value['item']}</li>";
+        // для вложенного меню здесь можно провести проверку если $item is_array и рекурсивно вызвать эту же функцию
+    }
+    return $resultStr . "</ul>";
+}
+
+echo renderMenu($menu);
